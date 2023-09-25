f31226992b83d83dc0a6286cf3adc32d9ef47f33
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-09-25 12:32:27 +0600

    some notes added

---
+++ notes.js
@@ -0,0 +1,4 @@
+// Step 1: npm init -y
+// Step 2: npm i express cors nodemon
+// Step 3: npm i -g express-generator
+// Step 4: express
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "ExpressJs" });
+  const count = 2 * 2;
+
+  res.render("index", { title: "ExpressJs", count: count });
 });
 
 module.exports = router;
